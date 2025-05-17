aeb9c3ec7f093a588cb1c75150c8ef3358ce3bf9
AncientSeer <nadakudutiganesh@gmail.com>
2025-05-17 18:09:37 +0530

    Changed background color and added a header

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,9 +3,12 @@ import Weather from './components/Weather'
 
 const App = () => {
   return (
+    <>
+    <h1 class="header">Weather App</h1>
     <div className='app'>
       <Weather/>
     </div>
+    </>
   )
 }
 
